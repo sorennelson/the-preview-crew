6403c7bde926178994fa71f9c54d8c88a1ad92f5
Soren Nelson <nelsonsorenc@gmail.com>
2025-10-24 22:53:14 -0500

    Initial commit

--- tmp.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-host = "http://127.0.0.1"
-port = "8000"
-movie_title = "What should I do on my trip to Vegas"
-
-res = requests.get(f"{host}:{port}/{movie_title}")
-print(res.json())
